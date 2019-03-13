2f1d4f494798e54434c47717587168ab813854ba
fenixbao92 <fenixbao92@gmail.com>
2019-03-13 23:46:28 +0800

    first commit

---
+++ src/main/java/com/fenxbao92/design/pattern/MyApplication.java
@@ -0,0 +1,16 @@
+package com.fenxbao92.design.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create by fenixbao92 on 2019/3/13.
+ */
+
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MyApplication.class, args);
+        System.out.println("启动成功");
+    }
+}
---
+++ src/main/java/com/fenxbao92/design/pattern/web/MyController.java
@@ -0,0 +1,20 @@
+package com.fenxbao92.design.pattern.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Create by fenixbao92 on 2019/3/13.
+ */
+@Controller
+public class MyController {
+
+    @RequestMapping(value = "/hello", method = {RequestMethod.GET})
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+
+}
